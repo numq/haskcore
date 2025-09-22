30e015d437e94c2924c45b72d46c73ae281cf5d4
numq <numq.dev@gmail.com>
2025-09-22 06:59:34 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/usecase/UseCase.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/haskcore/usecase/UseCase.kt
@@ -1,4 +1,4 @@
-package io.github.numq.haskcore.core.usecase
+package io.github.numq.haskcore.usecase
 
 interface UseCase<in Input, out Output> {
     suspend fun execute(input: Input): Result<Output>
