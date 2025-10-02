088f40ea641ca09714416e1f075112037707d909
numq <numq.dev@gmail.com>
2025-10-02 03:35:14 +0300

    Provide stack changes

--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/stack/StackTemplate.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/haskcore/stack/StackTemplate.kt
@@ -1,29 +1,11 @@
 package io.github.numq.haskcore.stack
 
 internal sealed interface StackTemplate {
-    companion object {
-        val values = setOf(Simple, Library, Executable, TestSuite, Full)
-    }
-
     val name: String
 
-    data object Simple : StackTemplate {
-        override val name = "simple"
+    data object Default : StackTemplate {
+        override val name = "new-template"
     }
 
-    data object Library : StackTemplate {
-        override val name = "library"
-    }
-
-    data object Executable : StackTemplate {
-        override val name = "executable"
-    }
-
-    data object TestSuite : StackTemplate {
-        override val name = "test-suite"
-    }
-
-    data object Full : StackTemplate {
-        override val name = "full"
-    }
+    data class Custom(override val name: String) : StackTemplate
 }
