c0e95bf69d8b787e46f5e5618bd1e7b54a9497ad
numq <numq.dev@gmail.com>
2025-11-04 02:08:35 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/workspace/Workspace.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/haskcore/workspace/Workspace.kt
@@ -1,6 +1,5 @@
 package io.github.numq.haskcore.workspace
 
-import io.github.numq.haskcore.stack.StackProject
 import io.github.numq.haskcore.timestamp.Timestamp
 import kotlinx.serialization.Serializable
 
@@ -8,7 +7,6 @@ import kotlinx.serialization.Serializable
 internal data class Workspace(
     val path: String,
     val name: String,
-    val stackProject: StackProject? = null,
     val createdAt: Timestamp = Timestamp.now(),
     val updatedAt: Timestamp = createdAt,
 )
