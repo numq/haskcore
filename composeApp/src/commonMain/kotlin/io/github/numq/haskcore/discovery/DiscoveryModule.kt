6e5200b95026675ec790d3cd0b9539400b5a2943
numq <numq.dev@gmail.com>
2025-11-09 00:22:31 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/discovery/DiscoveryModule.kt
+++
@@ -1,8 +0,0 @@
-package io.github.numq.haskcore.discovery
-
-import org.koin.dsl.bind
-import org.koin.dsl.module
-
-internal val discoveryModule = module {
-    single { DiscoveryService.Default() } bind DiscoveryService::class
-}
