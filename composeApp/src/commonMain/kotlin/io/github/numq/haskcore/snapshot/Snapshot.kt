bfe7b5162c1cd421bdec001b5ab0b67bc2993853
numq <numq.dev@gmail.com>
2025-11-18 02:57:55 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/snapshot/Snapshot.kt
+++
@@ -1,8 +0,0 @@
-package io.github.numq.haskcore.snapshot
-
-import io.github.numq.haskcore.workspace.Workspace
-
-internal data class Snapshot(
-    val lastOpenedWorkspacePath: String? = null,
-    val recentWorkspaces: List<Workspace> = emptyList(),
-)
