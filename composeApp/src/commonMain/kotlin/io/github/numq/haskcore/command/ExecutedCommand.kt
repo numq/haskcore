01babb63993e8c3b0e0ee72e9bc20427edfdab0b
numq <numq.dev@gmail.com>
2025-11-30 02:53:21 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/command/ExecutedCommand.kt
+++
@@ -1,6 +0,0 @@
-package io.github.numq.haskcore.command
-
-import io.github.numq.haskcore.output.OutputMessage
-import kotlinx.coroutines.flow.Flow
-
-internal data class ExecutedCommand(val command: String, val messages: Flow<OutputMessage>)
