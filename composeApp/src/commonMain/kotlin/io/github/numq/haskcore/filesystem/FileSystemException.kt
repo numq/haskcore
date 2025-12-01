32436e01706e10ea8b828540d289844bc99a7485
numq <numq.dev@gmail.com>
2025-12-02 00:47:30 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/filesystem/FileSystemException.kt
+++
@@ -1,3 +0,0 @@
-package io.github.numq.haskcore.filesystem
-
-internal data class FileSystemException(override val cause: Throwable?) : Exception(cause)
--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/filesystem/FileSystemModule.kt
+++
@@ -1,8 +0,0 @@
-package io.github.numq.haskcore.filesystem
-
-import org.koin.dsl.bind
-import org.koin.dsl.module
-
-internal val fileSystemModule = module {
-    single { FileSystemService.Default() } bind FileSystemService::class
-}
