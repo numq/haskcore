0e9d23ab4d0d7620005a99f5958f884d481c8e9f
numq <numq.dev@gmail.com>
2025-12-16 00:07:48 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/workspace/Workspace.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/haskcore/workspace/Workspace.kt
@@ -4,9 +4,19 @@ import io.github.numq.haskcore.timestamp.Timestamp
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class Workspace(
-    val path: String,
-    val name: String,
-    val createdAt: Timestamp = Timestamp.now(),
-    val updatedAt: Timestamp = createdAt,
-)
+internal sealed interface Workspace {
+    @Serializable
+    data object None : Workspace
+
+    @Serializable
+    data object Loading : Workspace
+
+    @Serializable
+    data class Loaded(
+        val path: String,
+        val name: String,
+        val createdAt: Timestamp = Timestamp.now(),
+        val updatedAt: Timestamp = createdAt,
+        val openedAt: Timestamp = createdAt,
+    ) : Workspace
+}
