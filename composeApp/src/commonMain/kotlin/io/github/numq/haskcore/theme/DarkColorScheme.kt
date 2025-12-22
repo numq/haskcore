531df64db34c95ba70447ef43f1822e422792912
numq <numq.dev@gmail.com>
2025-12-23 00:46:21 +0300

    Provide refactoring

--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/theme/DarkColorScheme.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/haskcore/theme/DarkColorScheme.kt
@@ -7,34 +7,17 @@ internal val DarkColorScheme = darkColorScheme(
     primary = Color(0xFFBD93F9),
     onPrimary = Color(0xFF282A36),
     primaryContainer = Color(0xFF44475A),
-    onPrimaryContainer = Color(0xFFF8F8F2),
-
     secondary = Color(0xFF8BE9FD),
     onSecondary = Color(0xFF282A36),
-    secondaryContainer = Color(0xFF44475A),
-    onSecondaryContainer = Color(0xFFF8F8F2),
-
     tertiary = Color(0xFFFF79C6),
-    onTertiary = Color(0xFF282A36),
-    tertiaryContainer = Color(0xFF44475A),
-    onTertiaryContainer = Color(0xFFF8F8F2),
-
     background = Color(0xFF282A36),
     onBackground = Color(0xFFF8F8F2),
-
-    surface = Color(0xFF44475A),
+    surface = Color(0xFF313442),
     onSurface = Color(0xFFF8F8F2),
-
-    surfaceVariant = Color(0xFF6272A4),
+    surfaceVariant = Color(0xFF44475A),
     onSurfaceVariant = Color(0xFFF8F8F2),
-
-    outline = Color(0xFFF1FA8C),
+    outline = Color(0xFF6272A4),
     outlineVariant = Color(0xFF50FA7B),
-
     error = Color(0xFFFF5555),
-    onError = Color(0xFF282A36),
-    errorContainer = Color(0xFF44475A),
-    onErrorContainer = Color(0xFFFFB86C),
-
     surfaceTint = Color(0xFFBD93F9)
 )
--- composeApp/src/commonMain/kotlin/io/github/numq/haskcore/theme/LightColorScheme.kt
+++ composeApp/src/commonMain/kotlin/io/github/numq/haskcore/theme/LightColorScheme.kt
@@ -7,34 +7,12 @@ internal val LightColorScheme = lightColorScheme(
     primary = Color(0xFF644AC9),
     onPrimary = Color(0xFFFFFFFF),
     primaryContainer = Color(0xFFEDEAF9),
-    onPrimaryContainer = Color(0xFF1F1F1F),
-
-    secondary = Color(0xFF036A96),
-    onSecondary = Color(0xFFFFFFFF),
-    secondaryContainer = Color(0xFFD6EBF7),
-    onSecondaryContainer = Color(0xFF141E25),
-
-    tertiary = Color(0xFFA3144D),
-    onTertiary = Color(0xFFFFFFFF),
-    tertiaryContainer = Color(0xFFFFD7EB),
-    onTertiaryContainer = Color(0xFF37001F),
-
-    background = Color(0xFFFFFBEB),
+    background = Color(0xFFF8F8F8),
     onBackground = Color(0xFF1F1F1F),
-
-    surface = Color(0xFFF8F6EF),
+    surface = Color(0xFFFFFFFF),
     onSurface = Color(0xFF1F1F1F),
-
-    surfaceVariant = Color(0xFFCECCC0),
-    onSurfaceVariant = Color(0xFF1F1F1F),
-
-    outline = Color(0xFF846E15),
-    outlineVariant = Color(0xFFA39514),
-
-    error = Color(0xFFCB3A2A),
-    onError = Color(0xFFFFFFFF),
-    errorContainer = Color(0xFFF5D4D1),
-    onErrorContainer = Color(0xFF5E0805),
-
+    surfaceVariant = Color(0xFFF0F0F0),
+    outline = Color(0xFF644AC9),
+    error = Color(0xFFDC143C),
     surfaceTint = Color(0xFF644AC9)
 )
