53916956611a2bdf34505ca66903ec48a5b0f617
numq <numq.dev@gmail.com>
2026-01-02 01:16:34 +0300

    Provide refactoring

---
+++ composeApp/src/commonMain/kotlin/io/github/numq/haskcore/buildsystem/custom/CustomBuildSystemService.kt
@@ -0,0 +1,6 @@
+package io.github.numq.haskcore.buildsystem.custom
+
+import io.github.numq.haskcore.buildsystem.BuildCommand
+import io.github.numq.haskcore.buildsystem.BuildSystemService
+
+internal class CustomBuildSystemService : BuildSystemService<BuildCommand.Custom>()
