2b0b1a5a4a0355d088bd5ac16a219b362fcca1a7
numq <numq.dev@gmail.com>
2026-02-04 01:10:04 +0300

    Provide configuration changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,30 +33,35 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-val platform = setOf("application", "core", "navigation", "ui")
+val baseModules = listOf("core")
+val platformModules = listOf("application", "navigation", "ui")
+val serviceModules = listOf(
+    "configuration", "document", "language", "project", "runtime", "session", "toolchain", "vfs"
+)
+val featureModules = listOf("editor", "explorer", "output", "settings", "workspace")
 
-val services = setOf("language", "runtime", "toolchain", "vfs")
-
-val modules = platform + services
-
-modules.forEach { module ->
-    include(":$module")
-
-    project(":$module").projectDir = when (module) {
-        in platform -> file("src/platform/$module")
-
-        else -> file("src/service/$module")
-    }
+baseModules.forEach { name ->
+    val path = ":$name"
+    include(path)
+    project(path).projectDir = file("src/$name")
 }
 
-val features = setOf("document", "explorer", "output", "settings", "window", "workspace")
+platformModules.forEach { name ->
+    val path = ":platform:$name"
+    include(path)
+    project(path).projectDir = file("src/platform/$name")
+}
 
-features.filterNot(modules::contains).forEach { feature ->
-    val core = ":$feature:core"
-    include(core)
-    project(core).projectDir = file("src/feature/$feature/core")
+serviceModules.forEach { name ->
+    val path = ":service:$name"
+    include(path)
+    project(path).projectDir = file("src/service/$name")
+}
 
-    val presentation = ":$feature:presentation"
-    include(presentation)
-    project(presentation).projectDir = file("src/feature/$feature/presentation")
+featureModules.forEach { name ->
+    val corePath = ":feature:$name:core"
+    val presentationPath = ":feature:$name:presentation"
+    include(corePath, presentationPath)
+    project(corePath).projectDir = file("src/feature/$name/core")
+    project(presentationPath).projectDir = file("src/feature/$name/presentation")
 }
