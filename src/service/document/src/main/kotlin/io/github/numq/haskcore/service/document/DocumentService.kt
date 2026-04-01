3d0b26dc47029748c75aebcd53e582aabc9218c3
numq <numq.dev@gmail.com>
2026-04-02 00:09:00 +0300

    Provide refactoring

--- src/service/document/src/main/kotlin/io/github/numq/haskcore/service/document/DocumentService.kt
+++ src/service/document/src/main/kotlin/io/github/numq/haskcore/service/document/DocumentService.kt
@@ -1,15 +1,8 @@
 package io.github.numq.haskcore.service.document
 
 import arrow.core.Either
-import io.github.numq.haskcore.core.timestamp.Timestamp
 
 interface DocumentService {
-    suspend fun getParentPath(path: String): Either<Throwable, String>
-
-    suspend fun getName(path: String): Either<Throwable, String>
-
-    suspend fun getLastModifiedTimestamp(path: String): Either<Throwable, Timestamp>
-
     suspend fun readDocument(path: String): Either<Throwable, Document>
 
     suspend fun saveDocument(path: String, content: String): Either<Throwable, Unit>
--- src/service/document/src/main/kotlin/io/github/numq/haskcore/service/document/LocalDocumentService.kt
+++ src/service/document/src/main/kotlin/io/github/numq/haskcore/service/document/LocalDocumentService.kt
@@ -1,34 +1,12 @@
 package io.github.numq.haskcore.service.document
 
 import arrow.core.Either
-import io.github.numq.haskcore.core.timestamp.Timestamp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.withContext
 import java.nio.file.Path
-import java.util.concurrent.TimeUnit
-import kotlin.io.path.getLastModifiedTime
-import kotlin.io.path.name
 
 internal class LocalDocumentService : DocumentService {
-    override suspend fun getParentPath(path: String) = Either.catch {
-        withContext(Dispatchers.IO) {
-            Path.of(path).parent.toString()
-        }
-    }
-
-    override suspend fun getName(path: String) = Either.catch {
-        withContext(Dispatchers.IO) {
-            Path.of(path).name
-        }
-    }
-
-    override suspend fun getLastModifiedTimestamp(path: String) = Either.catch {
-        withContext(Dispatchers.IO) {
-            Timestamp(nanoseconds = Path.of(path).getLastModifiedTime().to(TimeUnit.NANOSECONDS))
-        }
-    }
-
     override suspend fun readDocument(path: String) = Either.catch {
         withContext(Dispatchers.IO) {
             val file = Path.of(path).toFile()
