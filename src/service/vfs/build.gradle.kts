8cca401ce223081adf72c114e6fe2271e834b5ef
numq <numq.dev@gmail.com>
2026-04-05 00:15:28 +0300

    Provide refactoring

--- src/service/vfs/build.gradle.kts
+++ src/service/vfs/build.gradle.kts
@@ -4,14 +4,17 @@ plugins {
 
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.protobuf)
     implementation(libs.arrow.core)
     implementation(libs.koin.core)
+    implementation(libs.datastore)
     implementation(libs.directory.watcher)
     implementation(projects.core)
     testImplementation(libs.junit)
     testImplementation(libs.kotlin.test)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.mockk)
+    testImplementation(libs.turbine)
 }
 
 tasks.withType<Test>().configureEach {
