83efee939df0180226081050993bcbf63a2bbccb
numq <numq.dev@gmail.com>
2026-04-08 00:01:07 +0300

    Provide refactoring

--- src/service/runtime/src/main/kotlin/io/github/numq/haskcore/service/runtime/RuntimeRequest.kt
+++ src/service/runtime/src/main/kotlin/io/github/numq/haskcore/service/runtime/RuntimeRequest.kt
@@ -1,17 +1,62 @@
 package io.github.numq.haskcore.service.runtime
 
-sealed class RuntimeRequest(
-    val id: String, val name: String, val command: String, val arguments: List<String>, val workingDir: String? = null
-) {
-    class RunGHC(
-        id: String, name: String, path: String, arguments: List<String>
-    ) : RuntimeRequest(id = id, name = name, command = "runghc", arguments = listOf(path) + arguments)
+sealed interface RuntimeRequest {
+    private companion object {
+        const val STACK_COMMAND = "stack"
 
-    class Cabal(
-        id: String, name: String, arguments: List<String>
-    ) : RuntimeRequest(id = id, name = name, command = "cabal", arguments = arguments)
+        const val CABAL_COMMAND = "cabal"
 
-    class Stack(
-        id: String, name: String, arguments: List<String>
-    ) : RuntimeRequest(id = id, name = name, command = "stack", arguments = arguments)
+        const val GHC_COMMAND = "runghc"
+    }
+
+    val id: String
+
+    val name: String
+
+    val command: String
+
+    val arguments: List<String>
+
+    val workingDir: String?
+
+    val env: Map<String, String>
+
+    data class Stack(
+        override val id: String,
+        override val name: String,
+        override val arguments: List<String>,
+        override val workingDir: String?,
+        override val env: Map<String, String> = emptyMap()
+    ) : RuntimeRequest {
+        override val command = STACK_COMMAND
+    }
+
+    data class Cabal(
+        override val id: String,
+        override val name: String,
+        override val arguments: List<String>,
+        override val workingDir: String?,
+        override val env: Map<String, String> = emptyMap()
+    ) : RuntimeRequest {
+        override val command = CABAL_COMMAND
+    }
+
+    data class Ghc(
+        override val id: String,
+        override val name: String,
+        override val arguments: List<String>,
+        override val workingDir: String?,
+        override val env: Map<String, String> = emptyMap()
+    ) : RuntimeRequest {
+        override val command = GHC_COMMAND
+    }
+
+    data class Custom(
+        override val id: String,
+        override val name: String,
+        override val command: String,
+        override val arguments: List<String>,
+        override val workingDir: String?,
+        override val env: Map<String, String> = emptyMap()
+    ) : RuntimeRequest
 }
