dfcfb4b7a2a8e20cbc2a20f138c4392a51e99c4d
numq <numq.dev@gmail.com>
2026-04-14 00:12:08 +0300

    Provide refactoring

--- src/core/src/main/kotlin/io/github/numq/haskcore/core/di/ScopeQualifier.kt
+++ src/core/src/main/kotlin/io/github/numq/haskcore/core/di/ScopeQualifier.kt
@@ -3,9 +3,15 @@ package io.github.numq.haskcore.core.di
 import org.koin.core.qualifier.named
 
 object ScopeQualifier {
-    val Application = named("APPLICATION_PATH")
+    private const val APPLICATION_PATH = "APPLICATION_PATH"
 
-    val Project = named("PROJECT_PATH")
+    private const val PROJECT_PATH = "PROJECT_PATH"
 
-    val Document = named("DOCUMENT_PATH")
+    private const val DOCUMENT_PATH = "DOCUMENT_PATH"
+
+    val Application = named(APPLICATION_PATH)
+
+    val Project = named(PROJECT_PATH)
+
+    val Document = named(DOCUMENT_PATH)
 }
