b83cfe989cb07e68485b8e293069806006cbd4d2
numq <numq.dev@gmail.com>
2026-04-27 00:30:26 +0300

    Provide refactoring

--- src/feature/log/presentation/build.gradle.kts
+++ src/feature/log/presentation/build.gradle.kts
@@ -10,7 +10,6 @@ dependencies {
     implementation(libs.compose.foundation)
     implementation(libs.compose.material3)
     implementation(libs.compose.material.icons.extended)
-    implementation(projects.common.core)
     implementation(projects.common.presentation)
     implementation(projects.feature.log.core)
 }
