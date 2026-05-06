602d6eefcb4ca4c9daf2b41f6015392552e8359a
numq <numq.dev@gmail.com>
2026-05-07 01:18:06 +0300

    Provide refactoring

--- src/platform/overlay/src/main/kotlin/io/github/numq/haskcore/platform/overlay/dialog/file/FileDialog.kt
+++
@@ -1,5 +0,0 @@
-package io.github.numq.haskcore.platform.overlay.dialog.file
-
-interface FileDialog {
-    suspend fun pickDirectory(title: String): String?
-}
--- src/platform/overlay/src/main/kotlin/io/github/numq/haskcore/platform/overlay/menu/MenuState.kt
+++
@@ -1,9 +0,0 @@
-package io.github.numq.haskcore.platform.overlay.menu
-
-import androidx.compose.ui.geometry.Offset
-
-sealed interface MenuState {
-    data object Hidden : MenuState
-
-    data class Visible(val offset: Offset) : MenuState
-}
