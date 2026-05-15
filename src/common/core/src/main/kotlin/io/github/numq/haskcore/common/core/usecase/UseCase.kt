d1c0126e66e22005b06c2d752e04bc5e5f75a443
numq <numq.dev@gmail.com>
2026-05-16 02:04:57 +0300

    Provide refactoring

--- src/common/core/src/main/kotlin/io/github/numq/haskcore/common/core/usecase/UseCase.kt
+++ src/common/core/src/main/kotlin/io/github/numq/haskcore/common/core/usecase/UseCase.kt
@@ -4,8 +4,36 @@ import arrow.core.Either
 import arrow.core.raise.Raise
 import arrow.core.raise.either
 
-interface UseCase<in Input, out Output> {
+sealed interface UseCase<in Input, out Output> {
     suspend fun Raise<Throwable>.execute(input: Input): Output
 
-    suspend operator fun invoke(input: Input): Either<Throwable, Output> = either { execute(input) }
+    suspend operator fun invoke(input: Input): Either<Throwable, Output> = either { execute(input = input) }
+
+    interface Action : UseCase<Unit, Unit> {
+        suspend fun Raise<Throwable>.action()
+
+        override suspend fun Raise<Throwable>.execute(input: Unit): Unit = action()
+
+        suspend operator fun invoke(): Either<Throwable, Unit> = invoke(input = Unit)
+    }
+
+    interface Query<out Output> : UseCase<Unit, Output> {
+        suspend fun Raise<Throwable>.query(): Output
+
+        override suspend fun Raise<Throwable>.execute(input: Unit): Output = query()
+
+        suspend operator fun invoke(): Either<Throwable, Output> = invoke(input = Unit)
+    }
+
+    interface Command<in Input> : UseCase<Input, Unit> {
+        suspend fun Raise<Throwable>.command(input: Input)
+
+        override suspend fun Raise<Throwable>.execute(input: Input): Unit = command(input = input)
+    }
+
+    interface Exchange<in Input, out Output> : UseCase<Input, Output> {
+        suspend fun Raise<Throwable>.exchange(input: Input): Output
+
+        override suspend fun Raise<Throwable>.execute(input: Input): Output = exchange(input = input)
+    }
 }
