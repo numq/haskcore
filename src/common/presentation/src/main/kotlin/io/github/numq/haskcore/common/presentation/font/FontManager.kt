edd0d143ba83ec6ca236099650a50ea82204002e
numq <numq.dev@gmail.com>
2026-05-22 00:12:46 +0300

    Provide refactoring

--- src/common/presentation/src/main/kotlin/io/github/numq/haskcore/common/presentation/font/FontManager.kt
+++ src/common/presentation/src/main/kotlin/io/github/numq/haskcore/common/presentation/font/FontManager.kt
@@ -1,42 +1,7 @@
 package io.github.numq.haskcore.common.presentation.font
 
-import kotlinx.atomicfu.atomic
-import org.jetbrains.skia.Data
-import org.jetbrains.skia.FontMgr
 import org.jetbrains.skia.Typeface
-import java.io.InputStream
-import java.util.concurrent.ConcurrentHashMap
-import kotlin.io.readBytes
-import kotlin.use
 
-internal class FontManager : AutoCloseable {
-    private val _isClosed = atomic(false)
-
-    private val isClosed get() = _isClosed.value
-
-    private val typefaceCache = ConcurrentHashMap<String, Typeface>()
-
-    fun loadFont(fileName: String): Typeface {
-        check(!isClosed) { "Font manager is closed" }
-
-        return typefaceCache.computeIfAbsent(fileName) {
-            val resourcePath = listOf(FontResources.FONTS_PATH, fileName).joinToString(separator = "/")
-
-            val bytes = javaClass.classLoader.getResourceAsStream(resourcePath)?.use(InputStream::readBytes)
-
-            Data.makeFromBytes(checkNotNull(bytes) {
-                "Font resource not found: $resourcePath"
-            }).use { data ->
-                checkNotNull(FontMgr.default.makeFromData(data)) { "Failed to parse font data from $resourcePath" }
-            }
-        }
-    }
-
-    override fun close() {
-        if (!_isClosed.compareAndSet(expect = false, update = true)) return
-
-        typefaceCache.values.forEach(Typeface::close)
-
-        typefaceCache.clear()
-    }
+interface FontManager : AutoCloseable {
+    fun loadFont(fileName: String): Typeface
 }
