de72f84fc6034ccdd8307982204e65a107e3adac
numq <numq.dev@gmail.com>
2026-06-02 00:04:21 +0300

    Provide refactoring

--- src/common/core/src/main/kotlin/io/github/numq/haskcore/common/core/language/Language.kt
+++ src/common/core/src/main/kotlin/io/github/numq/haskcore/common/core/language/Language.kt
@@ -1,15 +1,35 @@
 package io.github.numq.haskcore.common.core.language
 
 sealed interface Language {
-    val column: Int?
+    companion object {
+        fun fromExtension(path: String) = path.substringAfterLast(".").let { extension ->
+            when (extension.lowercase()) {
+                in Haskell.extensions -> Haskell
+
+                else -> Undefined
+            }
+        }
+    }
+
+    val extensions: List<String>
+
+    val guidelineColumn: Int?
 
     data object Undefined : Language {
-        override val column = null
+        override val extensions = emptyList<String>()
+
+        override val guidelineColumn = null
     }
 
     data object Haskell : Language {
+        private const val HASKELL_EXTENSION_HS = "hs"
+
+        private const val HASKELL_EXTENSION_LHS = "lhs"
+
         private const val HASKELL_GUIDELINE_COLUMN = 80
 
-        override val column = HASKELL_GUIDELINE_COLUMN
+        override val extensions = listOf(HASKELL_EXTENSION_HS, HASKELL_EXTENSION_LHS)
+
+        override val guidelineColumn = HASKELL_GUIDELINE_COLUMN
     }
 }
