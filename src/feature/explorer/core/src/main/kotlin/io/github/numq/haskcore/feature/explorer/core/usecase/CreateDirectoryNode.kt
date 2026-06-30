5c3c44170e34c425ef9fdf675dfae619fed93753
numq <numq.dev@gmail.com>
2026-07-01 00:08:00 +0300

    Provide refactoring

---
+++ src/feature/explorer/core/src/main/kotlin/io/github/numq/haskcore/feature/explorer/core/usecase/CreateDirectoryNode.kt
@@ -0,0 +1,20 @@
+package io.github.numq.haskcore.feature.explorer.core.usecase
+
+import arrow.core.raise.Raise
+import io.github.numq.haskcore.common.core.usecase.UseCase
+import io.github.numq.haskcore.feature.explorer.core.ExplorerNode
+import io.github.numq.haskcore.service.vfs.VfsService
+import kotlin.io.path.Path
+import kotlin.io.path.absolutePathString
+
+class CreateDirectoryNode(
+    private val vfsService: VfsService
+) : UseCase.Command<CreateDirectoryNode.Input> {
+    data class Input(val node: ExplorerNode, val name: String)
+
+    override suspend fun Raise<Throwable>.command(input: Input) = with(input) {
+        vfsService.create(
+            path = Path(node.path, name).absolutePathString(), isDirectory = true
+        ).bind()
+    }
+}
