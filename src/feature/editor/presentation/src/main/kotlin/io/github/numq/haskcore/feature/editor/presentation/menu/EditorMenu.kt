395f6fb91369df0f647956d946721d52ddd6bdcc
numq <numq.dev@gmail.com>
2026-07-14 00:01:13 +0300

    Provide refactoring

--- src/feature/editor/presentation/src/main/kotlin/io/github/numq/haskcore/feature/editor/presentation/menu/EditorMenu.kt
+++
@@ -1,7 +0,0 @@
-package io.github.numq.haskcore.feature.editor.presentation.menu
-
-internal sealed interface EditorMenu {
-    data object Hidden : EditorMenu
-
-    data class Visible(val x: Float, val y: Float) : EditorMenu
-}
