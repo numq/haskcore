610f40c83e5e0303a8e1f69a8b9d053d8282000c
numq <numq.dev@gmail.com>
2026-08-23 03:24:33 +0300

    Provide refactoring

--- src/feature/explorer/presentation/src/main/kotlin/io/github/numq/haskcore/feature/explorer/presentation/feature/ExplorerState.kt
+++ src/feature/explorer/presentation/src/main/kotlin/io/github/numq/haskcore/feature/explorer/presentation/feature/ExplorerState.kt
@@ -1,5 +1,17 @@
 package io.github.numq.haskcore.feature.explorer.presentation.feature
 
+import io.github.numq.haskcore.feature.explorer.core.ExplorerNode
 import io.github.numq.haskcore.feature.explorer.core.ExplorerTree
+import io.github.numq.haskcore.feature.explorer.presentation.dialog.ExplorerDialog
+import io.github.numq.haskcore.feature.explorer.presentation.menu.ContextMenuState
 
-data class ExplorerState(val explorerTree: ExplorerTree, val selectedPath: String? = null)
+internal sealed interface ExplorerState {
+    data object Loading : ExplorerState
+
+    data class Ready(
+        val explorerTree: ExplorerTree,
+        val selectedNodes: List<ExplorerNode> = emptyList(),
+        val contextMenuState: ContextMenuState = ContextMenuState.Hidden,
+        val dialog: ExplorerDialog = ExplorerDialog.None,
+    ) : ExplorerState
+}
