eb83e1bfe680d47e1a0cc0a7ffa2ce318f1d92d6
numq <numq.dev@gmail.com>
2026-08-26 00:10:32 +0300

    Provide refactoring

--- src/feature/explorer/presentation/src/main/kotlin/io/github/numq/haskcore/feature/explorer/presentation/feature/ExplorerFeature.kt
+++ src/feature/explorer/presentation/src/main/kotlin/io/github/numq/haskcore/feature/explorer/presentation/feature/ExplorerFeature.kt
@@ -1,18 +1,12 @@
 package io.github.numq.haskcore.feature.explorer.presentation.feature
 
 import io.github.numq.haskcore.common.presentation.feature.Feature
-import io.github.numq.haskcore.feature.explorer.core.ExplorerRoot
-import io.github.numq.haskcore.feature.explorer.core.ExplorerTree
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 
-class ExplorerFeature(
-    private val path: String,
+internal class ExplorerFeature(
     private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default + SupervisorJob()), reducer: ExplorerReducer,
 ) : Feature<ExplorerState, ExplorerCommand, ExplorerEvent> by Feature(
-    initialState = ExplorerState(explorerTree = ExplorerTree.Loading(root = ExplorerRoot(path = path))),
-    scope = scope,
-    reducer = reducer,
-    ExplorerCommand.Initialize
+    initialState = ExplorerState.Loading, scope = scope, reducer = reducer, ExplorerCommand.Initialize
 )
