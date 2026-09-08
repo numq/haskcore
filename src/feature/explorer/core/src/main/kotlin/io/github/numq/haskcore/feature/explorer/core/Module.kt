318f9752f90d9b58b9ef2a54d49f8bbb21c06085
numq <numq.dev@gmail.com>
2026-09-09 00:11:33 +0300

    Provide refactoring

--- src/feature/explorer/core/src/main/kotlin/io/github/numq/haskcore/feature/explorer/core/Module.kt
+++ src/feature/explorer/core/src/main/kotlin/io/github/numq/haskcore/feature/explorer/core/Module.kt
@@ -3,7 +3,13 @@ package io.github.numq.haskcore.feature.explorer.core
 import androidx.datastore.core.DataStoreFactory
 import io.github.numq.haskcore.common.core.di.ScopeQualifier
 import io.github.numq.haskcore.common.core.di.scopedOwner
-import io.github.numq.haskcore.feature.explorer.core.usecase.*
+import io.github.numq.haskcore.feature.explorer.core.usecase.CollapseDirectory
+import io.github.numq.haskcore.feature.explorer.core.usecase.CreateDirectoryNode
+import io.github.numq.haskcore.feature.explorer.core.usecase.CreateFileNode
+import io.github.numq.haskcore.feature.explorer.core.usecase.ExpandDirectory
+import io.github.numq.haskcore.feature.explorer.core.usecase.ObserveExplorerTree
+import io.github.numq.haskcore.feature.explorer.core.usecase.OpenFile
+import io.github.numq.haskcore.feature.explorer.core.usecase.SaveExplorerPosition
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -25,9 +31,13 @@ val explorerFeatureCoreModule = module {
 
             val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
-            val dataStore = DataStoreFactory.create(serializer = ExplorerDataSerializer, scope = scope, produceFile = {
-                Path.of(projectPath, ".haskcore").also(Files::createDirectories).resolve("explorer.pb").toFile()
-            })
+            val dataStore = DataStoreFactory.create(
+                serializer = ExplorerDataSerializer,
+                scope = scope,
+                produceFile = {
+                    Path.of(projectPath, ".haskcore").also(Files::createDirectories)
+                        .resolve("explorer.pb").toFile()
+                })
 
             LocalExplorerDataSource(scope = scope, dataStore = dataStore)
         } bind ExplorerDataSource::class
@@ -39,15 +49,23 @@ val explorerFeatureCoreModule = module {
         } bind ExplorerService::class
 
         scopedOwner {
-            ObserveExplorerTree(root = get<ExplorerRoot>(), explorerService = get(), vfsService = get())
+            ObserveExplorerTree(
+                root = get<ExplorerRoot>(),
+                explorerService = get(),
+                vfsService = get()
+            )
         }
+
+        scopedOwner { CollapseDirectory(explorerService = get()) }
+
+        scopedOwner { CreateDirectoryNode(vfsService = get()) }
+
+        scopedOwner { CreateFileNode(vfsService = get()) }
+
+        scopedOwner { ExpandDirectory(explorerService = get()) }
 
         scopedOwner { OpenFile(projectService = get()) }
 
         scopedOwner { SaveExplorerPosition(explorerService = get()) }
-
-        scopedOwner { CollapseDirectory(explorerService = get()) }
-
-        scopedOwner { ExpandDirectory(explorerService = get()) }
     }
 }
