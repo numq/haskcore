32637022c162d7865792fb3cd9e6c1659d4dbecc
numq <numq.dev@gmail.com>
2026-09-23 00:08:46 +0300

    Provide refactoring

--- src/service/syntax/src/main/resources/queries/locals.scm
+++ src/service/syntax/src/main/resources/queries/locals.scm
@@ -1,8 +1,4 @@
-(export (variable)) @local.definition
 (signature name: (variable)) @local.definition
-(bind name: (variable)) @local.definition
 (function name: (variable)) @local.definition
-(decl/bind name: (variable)) @local.definition
-(pattern (variable)) @local.definition
-(expression (variable)) @local.reference
-(variable) @local.definition
+(pattern/variable) @local.definition
+(expression/variable) @local.reference
